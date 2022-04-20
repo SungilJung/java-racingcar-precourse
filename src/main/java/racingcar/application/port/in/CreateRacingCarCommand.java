64eb3c6b192e3c006eea6bc852cf9a9348ad9947
SungilJung <green5940@gmail.com>
2022-04-20 11:26:04 +0900

    feat: 주어진 입력으로 경주할 자동차를 생성하는 기능 구현

--- src/main/java/racingcar/application/port/in/CreateRacingCarCommand.java
+++ src/main/java/racingcar/application/port/in/CreateRacingCarCommand.java
@@ -1,5 +1,9 @@
 package racingcar.application.port.in;
 
+import java.util.ArrayList;
+import java.util.List;
+import racingcar.domain.RacingCarName;
+import racingcar.domain.RacingCarNames;
 import racingcar.util.RacingCarNameCheck;
 import racingcar.util.SelfValidating;
 
@@ -11,5 +15,16 @@ public class CreateRacingCarCommand extends SelfValidating<CreateRacingCarComman
     public CreateRacingCarCommand(String input) {
         this.input = input;
         super.validateSelf();
+    }
+
+    public RacingCarNames getRacingCarNames() {
+        String[] splitInput = input.split(",");
+        List<RacingCarName> carNameList = new ArrayList<>();
+
+        for(String name: splitInput) {
+            carNameList.add(new RacingCarName(name));
+        }
+
+        return new RacingCarNames(carNameList);
     }
 }
--- src/test/java/racingcar/application/port/in/CreateRacingCarCommandTest.java
+++ src/test/java/racingcar/application/port/in/CreateRacingCarCommandTest.java
@@ -1,9 +1,15 @@
 package racingcar.application.port.in;
 
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import racingcar.domain.RacingCarName;
+import racingcar.domain.RacingCarNames;
 
 class CreateRacingCarCommandTest {
 
@@ -11,5 +17,17 @@ class CreateRacingCarCommandTest {
     @ValueSource(strings = {"", "a,b,", "abcdef,ac", "ab,abcdefg"})
     void 예외_테스트(String input) {
         assertThatIllegalArgumentException().isThrownBy(() -> new CreateRacingCarCommand(input));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"ab,abc", "a,b", "a"})
+    void 자동차_이름_반환_테스트(String input) {
+
+        List<RacingCarName> carNameList = Arrays.stream(input.split(",")).map(RacingCarName::new)
+                .collect(Collectors.toList());
+
+        RacingCarNames expectedData = new RacingCarNames(carNameList);
+
+        assertEquals(expectedData, new CreateRacingCarCommand(input).getRacingCarNames());
     }
 }
