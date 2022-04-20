df858b78ae0ecbe7207c24f3411b9a0d885fdec9
SungilJung <green5940@gmail.com>
2022-04-20 20:28:31 +0900

    refactor: 객체 복사하는 함수 추가, RacingCar 이름이 동일한 경우 동일한 객체로 처리

--- src/main/java/racingcar/domain/RacingCar.java
+++ src/main/java/racingcar/domain/RacingCar.java
@@ -1,12 +1,40 @@
 package racingcar.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
+import lombok.EqualsAndHashCode;
 import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 
-@AllArgsConstructor
+@RequiredArgsConstructor
+@EqualsAndHashCode
 public class RacingCar {
     @NonNull
-    @Getter
-    final RacingCarName name;
+    private final RacingCarName name;
+
+    @EqualsAndHashCode.Exclude
+    private int moveCount = 0;
+
+    public void move() {
+        moveCount++;
+    }
+
+    @Override
+    public String toString() {
+        return name.getName() + " : " + toStringOfMoveCount();
+    }
+
+    private String toStringOfMoveCount() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < moveCount; i++) {
+            sb.append("-");
+        }
+
+        return sb.toString();
+    }
+
+    public static RacingCar newInstance(RacingCar car) {
+        RacingCar newInstance = new RacingCar(car.name);
+        newInstance.moveCount = car.moveCount;
+        return newInstance;
+
+    }
 }
