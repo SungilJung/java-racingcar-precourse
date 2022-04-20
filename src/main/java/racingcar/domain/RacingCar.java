e548572b7ccdfe4d23259683a161cc1fb966482c
SungilJung <green5940@gmail.com>
2022-04-20 11:23:54 +0900

    feat: 생성한 경주용 자동차를 저장, 가져오는 기능 구현

--- src/main/java/racingcar/domain/RacingCar.java
+++ src/main/java/racingcar/domain/RacingCar.java
@@ -2,9 +2,11 @@ package racingcar.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NonNull;
 
 @AllArgsConstructor
 public class RacingCar {
+    @NonNull
     @Getter
-    final String name;
+    final RacingCarName name;
 }
