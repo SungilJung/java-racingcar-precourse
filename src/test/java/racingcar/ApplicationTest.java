f0d09e6fc16abcfd58ecc2bb4d3b97c6f67a9c0b
SungilJung <green5940@gmail.com>
2022-04-22 09:24:52 +0900

    test: 레이싱 2번하는 테스트 추가

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -25,6 +25,17 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
+    void 전진_정지_2회() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("pobi,woni", "2");
+                    assertThat(output()).contains("pobi : --", "woni : ", "최종 우승자는 pobi 입니다.");
+                },
+                MOVING_FORWARD, STOP, MOVING_FORWARD, STOP
+        );
+    }
+
+    @Test
     void 이름에_대한_예외_처리() {
         assertSimpleTest(
             () -> {
